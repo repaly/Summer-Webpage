b25dbca86ce6a8d41dfdd8812d9cec74287e686f
repaly <dimsemin2008@yandex.ru>
2018-06-23 14:59:36 +0300

    ещё доработки

--- js/main.js
+++ js/main.js
@@ -62,6 +62,101 @@ jQuery(document).ready(function ($) {
 
 jQuery(document).ready(function ($) {
 
+    // var jssor_2_SlideoTransitions = [
+    //     [{b: -1, d: 1, o: -0.7}],
+    //     [{b: 900, d: 2000, x: -379, e: {x: 7}}],
+    //     [{b: 900, d: 2000, x: -379, e: {x: 7}}],
+    //     [{b: -1, d: 1, o: -1, sX: 2, sY: 2}, {
+    //         b: 0,
+    //         d: 900,
+    //         x: -171,
+    //         y: -341,
+    //         o: 1,
+    //         sX: -2,
+    //         sY: -2,
+    //         e: {x: 3, y: 3, sX: 3, sY: 3}
+    //     }, {b: 900, d: 1600, x: -283, o: -1, e: {x: 16}}]
+    // ];
+    //
+    // var jssor_2_options = {
+    //     $AutoPlay: 1,
+    //     $SlideDuration: 800,
+    //     $SlideEasing: $Jease$.$OutQuint,
+    //     $CaptionSliderOptions: {
+    //         $Class: $JssorCaptionSlideo$,
+    //         $Transitions: jssor_2_SlideoTransitions
+    //     },
+    //     $ArrowNavigatorOptions: {
+    //         $Class: $JssorArrowNavigator$
+    //     },
+    //     $BulletNavigatorOptions: {
+    //         $Class: $JssorBulletNavigator$
+    //     }
+    // };
+    //
+    // var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_2_options);
+    //
+    // /*#region responsive code begin*/
+    //
+    // var MAX_WIDTH = 3000;
+    //
+    // function ScaleSlider() {
+    //     var containerElement = jssor_2_slider.$Elmt.parentNode;
+    //     var containerWidth = containerElement.clientWidth;
+    //
+    //     if (containerWidth) {
+    //
+    //         var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+    //
+    //         jssor_2_slider.$ScaleWidth(expectedWidth);
+    //     }
+    //     else {
+    //         window.setTimeout(ScaleSlider, 30);
+    //     }
+    // }
+    //
+    // ScaleSlider();
+    //
+    // $(window).bind("load", ScaleSlider);
+    // $(window).bind("resize", ScaleSlider);
+    // $(window).bind("orientationchange", ScaleSlider);
+    // /*#endregion responsive code end*/
+});
+
+var config = {
+    apiKey: "AIzaSyDz7vYnW7BaiYuIh2dB3tvtzDNC6GFQDJQ",
+    authDomain: "volkonka-elling55.firebaseapp.com",
+    databaseURL: "https://volkonka-elling55.firebaseio.com",
+    projectId: "volkonka-elling55",
+    storageBucket: "volkonka-elling55.appspot.com",
+    messagingSenderId: "901445025885"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+database.ref('/rooms').once('value').then(function (snapshot) {
+    let insertingElements;
+    snapshot.forEach(function (childSnapShot) {
+        rooms.push(childSnapShot.val());
+    });
+    console.log(rooms);
+    rooms.forEach(function (index) {
+        let images = '';
+        index.images.forEach(function (image) {
+            images += '<img class="rounded" data-u="image" src=' + image + '/>'
+        });
+        let html = '                    <div data-p="225.00">\n' +
+                                        images +
+            '                        <div class="description-room">\n' +
+            '                            <button class="all-photos" data-toggle="modal" data-target="#exampleModal">Все фото</button>\n' +
+            '                            <h5>Аппартаменты с видом на море • от 1500р. за ночь</h5>\n' +
+            '                            <p>' + index.description + '</p>\n' +
+            '                        </div>\n' +
+            '                    </div>';
+        insertingElements += html;
+    });
+    $('#jssor_2 .room-slides').append(insertingElements);
+
     var jssor_2_SlideoTransitions = [
         [{b: -1, d: 1, o: -0.7}],
         [{b: 900, d: 2000, x: -379, e: {x: 7}}],
@@ -123,24 +218,15 @@ jQuery(document).ready(function ($) {
     /*#endregion responsive code end*/
 });
 
-var config = {
-    apiKey: "AIzaSyDz7vYnW7BaiYuIh2dB3tvtzDNC6GFQDJQ",
-    authDomain: "volkonka-elling55.firebaseapp.com",
-    databaseURL: "https://volkonka-elling55.firebaseio.com",
-    projectId: "volkonka-elling55",
-    storageBucket: "volkonka-elling55.appspot.com",
-    messagingSenderId: "901445025885"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+var rooms = [];
 
-database.ref('/rooms').once('value').then(function (snapshot) {
-    var rooms = [];
-    snapshot.forEach(function (childSnapShot) {
-        rooms.push(childSnapShot.val());
-    });
-    console.log(rooms);
-});
+function roomData(name) {
+    rooms.forEach(function(index) {
+        if (index.title == name) {
+            console.log(index);
+        }
+    })
+}
 
 database.ref("/config").once('value').then(function (snap) {
     const contactsData = snap.val().contacts;
